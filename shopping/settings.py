e229d34071623053cf9335db2ef08067d6428996
Luong Tran <tranthihienluong2003@gmail.com>
2025-02-19 15:41:49 +0700

    web homepage

--- shopping/settings.py
+++ shopping/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shopping.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "chorattot/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
