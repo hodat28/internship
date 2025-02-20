a299050d2131bef58846f549e1d97cc377c27316
Nhan <62550213+NhanPz01@users.noreply.github.com>
2025-02-20 15:58:49 +0700

    signin v2

--- chorattot/views.py
+++ chorattot/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 # Create your views here.
 
 def signup(request):
-    return render(request, 'signup.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'signup.html', context)
 
 def signin(request):
-    return render(request, 'signin.html')
+    context = {}
+    return render(request, 'signin.html', context)
 
 def forgotPassword(request):
     return render(request, 'forgotPassword.html')
