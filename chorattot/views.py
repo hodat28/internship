5fce391e94e3a4ff209f8a5c9c899a892a3941ed
Nhan <62550213+NhanPz01@users.noreply.github.com>
2025-02-27 10:27:32 +0700

    login ui v2

--- chorattot/views.py
+++ chorattot/views.py
@@ -6,18 +6,18 @@ from .models import *
 def home(request):
   return render(request, "home.html")
 
-def signup(request):
+def login(request):
     form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
     context = {'form':form}
-    return render(request, 'signup.html', context)
+    return render(request, 'login.html', context)
 
-def signin(request):
+def register(request):
     context = {}
-    return render(request, 'signin.html', context)
+    return render(request, 'register.html', context)
 
 def forgotPassword(request):
     return render(request, 'forgotPassword.html')
